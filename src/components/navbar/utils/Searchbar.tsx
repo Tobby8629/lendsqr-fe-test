dedd29dbae41a4352fae57ccfb4e4ecb6464bb01
tobby8629 <sarhmullah15@gmail.com>
2023-11-18 03:37:48 +0100

    Changes to searchBar and logout button

--- src/components/navbar/utils/Searchbar.tsx
+++ src/components/navbar/utils/Searchbar.tsx
@@ -1,12 +1,16 @@
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 const Searchbar = () => {
   return (
     <form className='nav-form'>
       <input type='text' placeholder='search for anything' />
-      <button type='submit'>ok</button>
+      <button type='submit'className='nav-form-btn'>
+        <FontAwesomeIcon icon={faSearch}/>
+      </button>
     </form>
   )
 }
 
-export default Searchbar
+export default Searchbar 
